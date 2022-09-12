66214c1e1ddbf7f9c7a395808ce415934e7280d6
Ilove-Heng <nangseakheng168@gmail.com>
2022-09-12 16:36:31 +0700

    magic link

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app">
+    hi
+    <router-link to="/login">Login</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+body {
+  @apply bg-gray-100 text-white;
+}
+
+.link {
+  @apply text-emerald-400 underline cursor-pointer;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="flex justify-center items-center text-red-500 h-screen"> login page</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
